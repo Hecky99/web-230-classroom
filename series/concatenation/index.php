0eed70fb046739a279f90962341d4d1402edab43
Brandon Heck <bhecky99@gmail.com>
2019-09-22 15:15:35 -0400

    Uploading all the stuff required for PHP Concatenation assignment

---
+++ series/concatenation/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$day = 'Wednesday';
+$date = 30;
+$year = 2012;
+
+//Both of the following lines of echo's is the different way of using your variables for double quotes and single quotes.
+
+/* echo 'The date is <strong>'.$day. ' '.$date. ' '.$year.'</strong>';
+
+echo "The date is $day $date $year"; */
+
+?>
